b8775d7fb6d80007a17dec39d4e500f7df70cc9f
pepramon-mafalda <pepramon@mafalda.local>
2024-12-16 19:48:12 +0100

    Contenedor personalizado con cron mediante sleep y gestión de UID y GID

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Utilizamos la imagen oficial nextcloud como base
+FROM nextcloud:stable
+
+# Copiar el script de inicio al contenedor
+COPY script_inicio.sh /script_inicio.sh
+
+# For inotify, y se actualizan los paquetes
+RUN chmod +x /script_inicio.sh && \
+    apt-get update && \
+    apt-get -y upgrade && \
+    apt autoremove -y && \
+    apt clean && \
+    rm -rf /var/lib/apt/lists/* 
+
+# Definir el script como entrypoint
+ENTRYPOINT ["/script_inicio.sh"]
+CMD ["apache2-foreground"]
---
+++ script_inicio.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Función para manejar una salida limpia del script
+salida_limpia() {
+    echo "Iniciando proceso de apagado..."
+    
+    # Detener cron
+    if [ -n "$CRON_PID" ]; then
+        kill -SIGTERM "$CRON_PID" 2>/dev/null
+        wait "$CRON_PID" 2>/dev/null
+    fi
+    
+    # Detener el proceso de Nextcloud
+    if kill -0 "$NC_PID" 2>/dev/null; then
+        echo "Deteniendo NextCloud..."
+        kill -SIGTERM "$NC_PID"
+        wait "$NC_PID"
+    fi
+    
+    echo "Apagado completo."
+    exit
+}
+
+# Configurar manejo de señales para salida ordenada
+# TERM, INT: Señales para detener el script de forma controlada (por ejemplo, Docker o Ctrl+C)
+# QUIT: Señal adicional de interrupción, útil en algunos sistemas
+# EXIT: Ejecuta la función `salida_limpia` para garantizar que los procesos se detengan correctamente
+trap "salida_limpia" EXIT SIGINT SIGTERM
+
+# Si se suministra un UID, se hace que apache de Nextcloud se ejecute con ese usuario
+if [ -n "$UID" ]; then
+    usermod -u "$UID" www-data
+fi
+
+# Si se suministra un GID, se hace que apache de Nextcloud se ejecute con ese grupo
+if [ -n "$GID" ]; then
+    groupmod -g "$GID" www-data
+fi
+
+# Se asegura que el directorio DATA de nextcloud es accesible en RW por apache
+if [ -n "$NEXTCLOUD_DATA_DIR" ]; then
+    chown -R www-data:www-data "$NEXTCLOUD_DATA_DIR"
+fi
+
+# Se lanza nginx con sus parámetros y se guarda el PID
+echo "Iniciando NextCloud..."
+/entrypoint.sh "$@" &
+NC_PID=$!
+
+# Se configura una espera para ejecutar el cron
+if [ -n "$TIEMPO_ESPERA" ]; then
+    while true; do
+        echo "Ejecutando Cron y esperando $TIEMPO_ESPERA"
+        su -s /bin/bash -c "php -f /var/www/html/cron.php" www-data
+        sleep "$TIEMPO_ESPERA"
+    done &
+    CRON_PID=$!
+fi
+
+# Esperar a que los procesos terminen
+wait "$NC_PID"
+EXIT_CODE=$?
+
+if [ -n "$CRON_PID" ]; then
+    wait "$CRON_PID"
+fi
+
+# Informar sobre la salida del script
+echo "Saliendo con código ${EXIT_CODE}"
+exit ${EXIT_CODE}
+
